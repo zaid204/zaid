ed01c30043edb686b319e734ae3f9ca0a2e50b6b
zaid204 <104628637+zaid204@users.noreply.github.com>
2024-11-20 05:33:12 +0300

    script.js

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ function play() {
   }
 }
 
-// Add an event listener to hide the pause icon
-const playerContainer = document.querySelector(".plyr"); // The main player container
+// Hide the controls (pause button) on screen click
+const playerContainer = document.querySelector(".plyr");
+
 if (playerContainer) {
   playerContainer.addEventListener("click", () => {
     const controls = playerContainer.querySelector(".plyr__controls");
     if (controls) {
-      controls.classList.toggle("hidden"); // Add/remove the hidden class to toggle visibility
+      controls.style.opacity = controls.style.opacity === "0" ? "1" : "0";
     }
   });
 }
